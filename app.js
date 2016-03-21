989e2e360e9418f508286f065dd6ecb821204a90
kevinzou <kaenry@163.com>
2016-03-21 12:45:05 +0800

    add jquery, client add jade

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
 app.get('/clients/add', routes.clients.add);
+app.get('/clients/testConnect', routes.clients.testConnect);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/clients.js
+++ routes/clients.js
@@ -1,3 +1,12 @@
 exports.add = function(req, res, next) {
   res.render('clients/add', {title: '添加实例'});
 };
+exports.testConnect = function(req, res, next) {
+  var redis = require('redis');
+  var client = redis.createClient();
+
+  client.on('ready', function(){
+    console.log('ready')
+  })
+  res.send(client.server_info.redis_version);
+};
